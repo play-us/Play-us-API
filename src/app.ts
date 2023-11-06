7c28f33930e3337c6399110a8f47befc6b9ec730
csw <csw5050@hanmail.net>
2023-11-06 16:01:58 +0900

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 const app:express.Application = express();
+
+//cors
+//app.use(cors()); //모든 접근 허용
+app.use(cors({ origin: 'http://localhost:3000'})); //local
 
 //swagger
 const swaggerUi = require('swagger-ui-express');
