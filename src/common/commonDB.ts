7bd98b5f4d16a4debba48946ad344367ac44ef96
csw <csw5050@hanmail.net>
2023-11-16 20:11:01 +0900

    api

--- src/common/commonDB.ts
+++ src/common/commonDB.ts
@@ -1,24 +1,23 @@
 const getSysCode = (syscdCd:string, classCd:string, rel01Data:string, rel02Data:string, useYn:string)=>{
-    let sql = 'SELECT syscd_cd' +
-    ',syscd_nm ' +
-    ',class_cd ' +
-    ',sort ' +
-    ',rel01_data ' +
-    ',rel02_data ' +
-    ',use_yn ' +
-    ',remark_txt ' +
-    ',insert_datetime ' +
-    ',update_datetime ' +
-    'FROM sys_code ' + 
-    ' WHERE 1=1'; 
-    if(syscdCd) sql = sql + " and syscd_cd = '" + syscdCd + "'";
-    if(classCd) sql =  sql + " and class_cd = '" + classCd + "'";
-    if(rel01Data) sql =  sql + " and rel01_data = '" + rel01Data + "'";
-    if(rel02Data) sql =  sql + " and rel02_data = '" + rel02Data + "'";
-    if(useYn) sql =  sql + " and use_yn = '" + useYn + "'";
-    sql =  sql + ' order by sort'
+    let sql = `SELECT syscd_cd 
+    ,syscd_nm  
+    ,class_cd  
+    ,sort  
+    ,rel01_data  
+    ,rel02_data  
+    ,use_yn  
+    ,remark_txt  
+    ,insert_datetime  
+    ,update_datetime  
+    FROM sys_code   
+     WHERE 1=1`; 
+    if(syscdCd) sql = sql +  ` and syscd_cd = '${syscdCd}'`;
+    if(classCd) sql =  sql +  ` and class_cd = '${classCd}'`;
+    if(rel01Data) sql =  sql +  ` and rel01_data = '${rel01Data}'`;
+    if(rel02Data) sql =  sql +  ` and rel02_data = '${rel02Data}'`;
+    if(useYn) sql =  sql + ` and use_yn = '${useYn}'`;
+    sql =  sql + ` order by sort`
     return sql;
 };
-
 
 module.exports = {getSysCode};
