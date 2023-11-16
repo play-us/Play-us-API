61e4f22816886c88b833e869fb6bb7f9d448b0c5
csw <csw5050@hanmail.net>
2023-11-16 19:53:15 +0900

    api

--- src/community/communityDB.ts
+++ src/community/communityDB.ts
@@ -1,181 +1,183 @@
 // 커뮤니티 리스트 조회
 const getCommunityList = (fieldTp:string, area:string, searchTxt:string, pageStart:number, pageEnd: number, email:string)=>{
-    let sql = 'SELECT commu_id' +
-        ', commu_title ' +
-        ', commu_txt ' +
-        ', email ' +
-        ', area ' +
-        ', dead_line ' +
-        ', member_cnt ' +
-        ', field_tp ' +
-        ", (select count(1) from community_wish x where x.commu_id = a.commu_id) as wish_cnt " +
-        ", (case when (select distinct email from community_wish x where x.commu_id = a.commu_id and x.email = '" + email + "' ) is not null then '1' else '0' end ) as wish_yn " + 
-        ", (select count(1) from community_comment x where x.commu_id = a.commu_id) as comment_cnt " +
-        ', insert_datetime ' +
-        ', update_datetime ' +
-        'FROM community a' + 
-        ' WHERE 1=1'; 
-    if(fieldTp) sql = sql + " and field_tp = '" + fieldTp + "'";
-    if(area) sql = sql + " and area = '" + area + "'";
-    if(searchTxt) sql = sql + " and commu_title like '%" + searchTxt + "%'";
-    //if(email) sql = sql + " and email '" + email + "'";
+    let sql = `SELECT commu_id
+    , commu_title 
+    , commu_txt 
+    , email 
+    , area 
+    , dead_line 
+    , member_cnt 
+    , field_tp 
+    , (select count(1) from community_wish x where x.commu_id = a.commu_id) as wish_cnt
+    , (case when (select distinct email from community_wish x where x.commu_id = a.commu_id and x.email = '${email}' ) is not null then 1 else 0 end ) as wish_yn
+    , (select count(1) from community_comment x where x.commu_id = a.commu_id) as comment_cnt
+    , insert_datetime 
+    , update_datetime 
+    FROM community a 
+     WHERE 1=1`; 
+    if(fieldTp) sql = sql + ` and field_tp = '${fieldTp}'`;
+    if(area) sql = sql + ` and area = '${area}'`;
+    if(searchTxt) sql = sql + ` and commu_title like '%${searchTxt}%'`;
     sql =  sql + ' order by insert_datetime desc'
     if(pageStart && pageEnd){
-        sql = sql + ' limit ' + pageStart + ', ' + pageEnd;
+        sql = sql + ` limit ${pageStart}, ${pageEnd}`;
     }
     return sql;
 };
 
 //커뮤니티상세 조회
 const getCommunityDetail = (commuId:string, email:string)=>{
-    let sql = 'SELECT a.commu_id' +
-        ', a.commu_title ' +
-        ', a.commu_txt ' +
-        ', a.email ' +
-        ", (select syscd_nm from sys_code x where x.class_cd = 'SYS002' and x.syscd_cd = a.field_tp) as field_tp " +
-        ", (select syscd_nm from sys_code x where x.class_cd = 'SYS006' and x.syscd_cd = a.area) as area " +
-        ', a.dead_line ' +
-        ', a.member_cnt ' +
-        ', a.remark_txt ' +
-        ', a.insert_datetime ' +
-        ', a.update_datetime ' +
-        ", (select count(1) from community_wish x where x.commu_id = a.commu_id) as wish_cnt " +
-        ", (case when b.wish_id is not null then '1' else '0' end) as wish_yn " +
-        "from community a " +
-        "left outer join community_wish b on b.commu_id = a.commu_id and b.email = '" + email + "' " +
-        "where 1=1 ";
-    if(commuId) sql = sql + " and a.commu_id = '" + commuId  + "'";
+    let sql = `SELECT a.commu_id 
+        , a.commu_title  
+        , a.commu_txt  
+        , a.email  
+        , (select syscd_nm from sys_code x where x.class_cd = 'SYS002' and x.syscd_cd = a.field_tp) as field_tp  
+        , (select syscd_nm from sys_code x where x.class_cd = 'SYS006' and x.syscd_cd = a.area) as area  
+        , a.dead_line  
+        , a.member_cnt  
+        , a.remark_txt  
+        , a.insert_datetime  
+        , a.update_datetime  
+        , (select count(1) from community_wish x where x.commu_id = a.commu_id) as wish_cnt  
+        , (case when b.wish_id is not null then '1' else '0' end) as wish_yn  
+        from community a  
+        left outer join community_wish b on b.commu_id = a.commu_id and b.email = '${email}'  
+        where 1=1 `;
+    if(commuId) sql = sql + ` and a.commu_id = '${commuId}'`;
     return sql;
 };
 
 //커뮤니티 등록
 const insertCommunity = (commuTitle:string, commuTxt:string, email:string, area:string, deadLine:Date, memberCnt:number, fieldTp:string) =>{
-    const sql = "INSERT INTO community (" + 
-    "commu_id" + ", " +
-    "commu_title" + ", " +
-    "commu_Txt" + ", " +
-    "email" + ", " +
-    "area" + ", " +
-    "dead_line" + ", " +
-    "member_cnt" + ", " +
-    "field_tp" + ", " +
-    "insert_datetime" + ", " +
-    "update_datetime" + ") " +
-    "VALUES ( " +
-    "(select ifnull(max(CAST(b.commu_id AS unsigned)) + 1, 1) from community b), '" + 
-    commuTitle + "', '" +
-    commuTxt + "', '" +
-    email + "', '" +
-    area + "', '" +
-    deadLine + "', '" +
-    memberCnt + "', '" +
-    fieldTp + "', " +
-    "DATE_ADD(NOW(), INTERVAL 9 HOUR), DATE_ADD(NOW(), INTERVAL 9 HOUR) )";
+    const sql = `INSERT INTO community (  
+    commu_id,  
+    commu_title,  
+    commu_Txt,  
+    email,  
+    area,  
+    dead_line,  
+    member_cnt,  
+    field_tp,  
+    insert_datetime,  
+    update_datetime  )  
+    VALUES (  
+    (select ifnull(max(CAST(b.commu_id AS unsigned)) + 1, 1) from community b),  
+    '${commuTitle}', 
+    '${commuTxt}', 
+    '${email}', 
+    '${area}', 
+    '${deadLine}', 
+    '${memberCnt}', 
+    '${fieldTp}',  
+    DATE_ADD(NOW(), INTERVAL 9 HOUR),
+    DATE_ADD(NOW(), INTERVAL 9 HOUR) 
+    )`;
     return sql;
 }
 
 //커뮤니티 수정
 const updateCommunity = (commuId:string, commuTitle:string, commuTxt:string, email:string, area:string, deadLine:Date, memberCnt:number, fieldTp:string) =>{
-    const sql = "UPDATE community set " + 
-    "commu_title = '" + commuTitle + "', " +
-    "commu_txt = '" + commuTitle + "', " +
-    "email = '" + email + "', " +
-    "area = '" + area + "', " +
-    "dead_line = '" + deadLine + "', " +
-    "member_cnt = " + memberCnt + ", " +
-    "field_tp = '" + fieldTp + "', " +
-    "update_datetime = DATE_ADD(NOW(), INTERVAL 9 HOUR) " +
-    "WHERE commu_id = '" + commuId + "'";
+    const sql = `UPDATE community set 
+    commu_title = '${commuTitle}',  
+    commu_txt = '${commuTitle}',  
+    email = '${email}',  
+    area = '${area}',  
+    dead_line = '${deadLine}',  
+    member_cnt = ${memberCnt},  
+    field_tp = '${fieldTp}',  
+    update_datetime = DATE_ADD(NOW(), INTERVAL 9 HOUR)  
+    WHERE commu_id = '${commuId}'`;
     return sql;
 }
 
 //커뮤니티 삭제
 const deleteCommunity = (commuId:string)=> {
-    const sql = "DELETE FROM community where commu_id = '" + commuId + "'";
+    const sql = `DELETE FROM community where commu_id = '${commuId}'`;
     return sql;
 }
 
 //커뮤니티 댓글리스트 조회
 const getCommunityCommentList = (commuId:string, email:string, pageStart:number, pageEnd: number)=>{
-    let sql = 'SELECT a.comment_id' +
-        ', a.commu_id ' +
-        ', a.comment_seq ' +
-        ', a.email ' +
-        ', (select x.name from member x where x.email = a.email) as name' +
-        ', a.comment_txt ' +
-        ', a.insert_datetime ' +
-        ', a.update_datetime ' +
-        "from community_comment a " +
-        "where 1=1 ";
-    if(commuId) sql = sql + " and a.commu_id = '" + commuId  + "'";
-    if(email) sql = sql + " and a.email = '" + email  + "'";
-    sql = sql + " order by a.insert_datetime desc, comment_id desc ";
+    let sql = `SELECT a.comment_id 
+        , a.commu_id  
+        , a.comment_seq  
+        , a.email  
+        , (select x.name from member x where x.email = a.email) as name 
+        , a.comment_txt  
+        , a.insert_datetime  
+        , a.update_datetime  
+        from community_comment a 
+        where 1=1 `;
+    if(commuId) sql = sql +  ` and a.commu_id = '${commuId}'`;
+    if(email) sql = sql + ` and a.email = '${email}'`;
+    sql = sql + ` order by a.insert_datetime desc, comment_id desc `;
     if(pageStart && pageEnd){
-        sql = sql + ' limit ' + pageStart + ', ' + pageEnd;
+        sql = sql + ` limit ${pageStart}, ${pageEnd}`;
     }
     return sql;
 };
 
 //커뮤니티댓글 등록
 const insertCommunityComment = (commuId:string, commentTxt:string, email:string) =>{
-    const sql = "INSERT INTO community_comment ( " +
-    "comment_id, " +
-    "commu_id, " +
-    "comment_seq, " +
-    "email, " +
-    "comment_txt, " +
-    "insert_datetime, " +
-    "update_datetime ) " +
-    "VALUES (" + 
-    "(select ifnull(max(CAST(b.comment_id AS unsigned)) + 1, 1) from community_comment b), '" + 
-    commuId + "', " + 
-    "(select ifnull(max(CAST(c.comment_seq AS unsigned)) + 1, 1) from community_comment c where c.commu_id = '" + commuId + "'), '" + 
-    email + "', '" +
-    commentTxt + "', " +
-    "DATE_ADD(NOW(), INTERVAL 9 HOUR), DATE_ADD(NOW(), INTERVAL 9 HOUR)) ";
+    const sql = `INSERT INTO community_comment (  
+    comment_id,  
+    commu_id,  
+    comment_seq,  
+    email,  
+    comment_txt,  
+    insert_datetime,  
+    update_datetime )  
+    VALUES (  
+    (select ifnull(max(CAST(b.comment_id AS unsigned)) + 1, 1) from community_comment b),  
+    '${commuId}',   
+    (select ifnull(max(CAST(c.comment_seq AS unsigned)) + 1, 1) from community_comment c where c.commu_id = '${commuId}'),  
+    '${email}', 
+    '${commentTxt}',  
+    DATE_ADD(NOW(), INTERVAL 9 HOUR),
+    DATE_ADD(NOW(), INTERVAL 9 HOUR)) `;
     return sql;
 }
 
 //커뮤니티댓글 수정
 const updateCommunityComment = (commentId:string, commentTxt:string) =>{
-    const sql = "UPDATE community_comment set " +
-    "comment_txt = '" + commentTxt + "'," +
-    "update_datetime = DATE_ADD(NOW(), INTERVAL 9 HOUR) "  +
-    "WHERE comment_id = '" + commentId + "'";
+    const sql = `UPDATE community_comment set 
+    comment_txt = '${commentTxt}',
+    update_datetime = DATE_ADD(NOW(), INTERVAL 9 HOUR)  
+    WHERE comment_id = '${commentId}'`;
     return sql;
 }
 
 //커뮤니티댓글 삭제
 const deleteCommunityComment = (commentId:string)=> {
-    const sql = "DELETE FROM community_comment where comment_id = '" + commentId + "'";
+    const sql = `DELETE FROM community_comment where comment_id = '${commentId}'`;
     return sql;
 }
 //커뮤니티댓글 삭제ALL
 const deleteCommunityCommentAll = (commuId:string)=> {
-    const sql = "DELETE FROM community_comment where commu_id = '" + commuId + "'";
+    const sql = `DELETE FROM community_comment where commu_id = '${commuId}'`;
     return sql;
 }
 
 //커뮤니티좋아요 insert
 const insertCommunityWish = (commuId:string, email:string)=> {
-    const sql = "insert into community_wish ( wish_id, commu_id, email, insert_datetime, update_datetime  )" + 
-    "SELECT (select ifnull(max(CAST(b.wish_id AS unsigned)) + 1, 1) from community_wish b) ,'" + commuId + "', '" + email + "', " +
-    " DATE_ADD(NOW(), INTERVAL 9 HOUR), DATE_ADD(NOW(), INTERVAL 9 HOUR) " +
-    " FROM DUAL WHERE NOT EXISTS (SELECT email FROM community_wish where commu_id = '" + commuId + "' and email = '" + email + "')"
+    const sql = `insert into community_wish ( wish_id, commu_id, email, insert_datetime, update_datetime  ) 
+    SELECT (select ifnull(max(CAST(b.wish_id AS unsigned)) + 1, 1) from community_wish b) ,'${commuId}', '${email}',
+     DATE_ADD(NOW(), INTERVAL 9 HOUR), DATE_ADD(NOW(), INTERVAL 9 HOUR)
+     FROM DUAL WHERE NOT EXISTS (SELECT email FROM community_wish where commu_id = '${commuId}' and email = '${email}')`;
     return sql;
 }
 
 //커뮤니티좋아요 delete
 const deleteCommunityWish = (commuId:string, email:string)=>{
-    const sql = "delete from community_wish" +
-        "where commu_id = '" + commuId + "' " + 
-        " and email = '" + email + "'";
+    const sql = `delete from community_wish 
+        where commu_id = '${commuId}'   
+         and email = '${email}'`;
     return sql;
 }
 
 //커뮤니티좋아요 삭제ALL
 const deleteCommunityWishAll = (commuId:string)=> {
-    const sql = "DELETE FROM community_wish where commu_id = '" + commuId + "'";
+    const sql = `DELETE FROM community_wish where commu_id = '${commuId}'`;
     return sql;
 }
 module.exports = {getCommunityList, getCommunityDetail, insertCommunity, updateCommunity, deleteCommunity, getCommunityCommentList, insertCommunityComment, updateCommunityComment, deleteCommunityComment, deleteCommunityCommentAll, insertCommunityWish, deleteCommunityWish, deleteCommunityWishAll};
--- src/field/fieldDB.ts
+++ src/field/fieldDB.ts
@@ -2,48 +2,48 @@ import { StringMappingType } from "typescript";
 
 //구장리스트 조회
 const getFieldList = (fieldId:string, fieldTp:string, area:string, searchTxt:string, sort: string, pageStart:number, pageEnd: number)=>{
-    let sql = "select a.field_id "+
-	    ", a.field_nm "+
-        ", (select syscd_nm from sys_code x where x.class_cd = 'SYS002' and x.syscd_cd = a.field_tp) as field_tp " +
-        ", (select syscd_nm from sys_code x where x.class_cd = 'SYS006' and x.syscd_cd = a.area) as area " +
-        ", a.addr " +
-        ", a.lat " +
-        ", a.lng " +
-        ", a.opening_hours " +
-        ", a.closing_hours " +
-        ", a.price " +
-        ", a.hours " +
-        ", a.note " +
-        ", a.size " +
-        ", a.swrm_yn " +
-        ", a.parking_tp " +
-        ", a.rental_sup " +
-        ", a.use_yn " +
-        ", a.remark_txt " +
-        ", a.insert_datetime " +
-        ", a.insert_datetime " +
-        ", b.img_id " +
-        ", b.img_seq " +
-        ", b.img_name " +
-        ", b.img_url " +
-        ", b.img_sort " +
-        ", (select count(1) from field_like x where x.field_id = a.field_id) as like_cnt " +
-        ", (select count(1) from field_review x where x.field_id = a.field_id) as review_cnt " +
-        ", (select count(1) from reservation x where x.field_id = a.field_id) as resv_cnt " +
-        "from field a " +
-        "left outer join field_image b on b.field_id = a.field_id and b.img_sort = 1 " +
-        "where 1=1 ";
-    if(fieldId) sql = sql + " and field_id = '" + fieldId + "'";
-    if(fieldTp) sql = sql + " and field_tp = '" + fieldTp + "'";
-    if(area) sql = sql + " and area = '" + area + "'";
-    if(searchTxt) sql = sql + " and field_nm like '%" + searchTxt + "%'";
+    let sql = `select a.field_id 
+	    , a.field_nm  
+        , (select syscd_nm from sys_code x where x.class_cd = 'SYS002' and x.syscd_cd = a.field_tp) as field_tp  
+        , (select syscd_nm from sys_code x where x.class_cd = 'SYS006' and x.syscd_cd = a.area) as area  
+        , a.addr  
+        , a.lat  
+        , a.lng  
+        , a.opening_hours  
+        , a.closing_hours  
+        , a.price  
+        , a.hours  
+        , a.note  
+        , a.size  
+        , a.swrm_yn  
+        , a.parking_tp  
+        , a.rental_sup  
+        , a.use_yn  
+        , a.remark_txt  
+        , a.insert_datetime  
+        , a.insert_datetime  
+        , b.img_id  
+        , b.img_seq  
+        , b.img_name  
+        , b.img_url  
+        , b.img_sort  
+        , (select count(1) from field_like x where x.field_id = a.field_id) as like_cnt  
+        , (select count(1) from field_review x where x.field_id = a.field_id) as review_cnt  
+        , (select count(1) from reservation x where x.field_id = a.field_id) as resv_cnt  
+        from field a  
+        left outer join field_image b on b.field_id = a.field_id and b.img_sort = 1  
+        where 1=1 `;
+    if(fieldId) sql = sql + ` and field_id = '${fieldId}'`;
+    if(fieldTp) sql = sql + ` and field_tp = '${fieldTp}'`;
+    if(area) sql = sql + ` and fielaread_tp = '${area}'`;
+    if(searchTxt) sql = sql + ` and field_nm like '%${searchTxt}%' `;
     if(sort && sort === '1') {
         sql = sql + ' order by resv_cnt desc, insert_datetime desc, field_nm';
     } else {
         sql = sql + ' order by insert_datetime desc, field_nm ';
     }
     if(pageStart && pageEnd){
-        sql = sql + ' limit ' + pageStart + ', ' + pageEnd;
+        sql = sql + ` limit ${pageStart}, ${pageEnd}`;
     }
     
     return sql;
@@ -51,118 +51,118 @@ const getFieldList = (fieldId:string, fieldTp:string, area:string, searchTxt:str
 
 //구장상세 조회
 const getFieldDetail = (fieldId:string, email:string)=>{
-    let sql = "select distinct a.field_id "+
-        ", a.field_nm "+
-        ", (select syscd_nm from sys_code x where x.class_cd = 'SYS002' and x.syscd_cd = a.field_tp) as field_tp " +
-        ", (select syscd_nm from sys_code x where x.class_cd = 'SYS006' and x.syscd_cd = a.area) as area " +
-        ", a.addr " +
-        ", a.lat " +
-        ", a.lng " +
-        ", a.opening_hours " +
-        ", a.closing_hours " +
-        ", a.price " +
-        ", a.hours " +
-        ", a.note " +
-        ", a.size " +
-        ", a.swrm_yn " +
-        ", a.parking_tp " +
-        ", a.rental_sup " +
-        ", a.use_yn " +
-        ", a.remark_txt " +
-        ", a.insert_datetime " +
-        ", a.insert_datetime " +
-        ", (select count(1) from field_like x where x.field_id = a.field_id) as like_cnt " +
-        ", (select count(1) from field_review x where x.field_id = a.field_id) as review_cnt " +
-        ", (case when b.like_id is not null then '1' else '0' end) as like_yn " +
-        "from field a " +
-        "left outer join field_like b on b.field_id = a.field_id and b.email = '" + email + "' " + 
-        "where 1=1 and a.field_id = '1'";
-    if(fieldId) sql = sql + " and a.field_id = '" + fieldId  + "'";
+    let sql = `select distinct a.field_id 
+        , a.field_nm 
+        , (select syscd_nm from sys_code x where x.class_cd = 'SYS002' and x.syscd_cd = a.field_tp) as field_tp  
+        , (select syscd_nm from sys_code x where x.class_cd = 'SYS006' and x.syscd_cd = a.area) as area  
+        , a.addr  
+        , a.lat  
+        , a.lng  
+        , a.opening_hours  
+        , a.closing_hours  
+        , a.price  
+        , a.hours  
+        , a.note  
+        , a.size  
+        , a.swrm_yn  
+        , a.parking_tp  
+        , a.rental_sup  
+        , a.use_yn  
+        , a.remark_txt  
+        , a.insert_datetime  
+        , a.insert_datetime  
+        , (select count(1) from field_like x where x.field_id = a.field_id) as like_cnt  
+        , (select count(1) from field_review x where x.field_id = a.field_id) as review_cnt  
+        , (case when b.like_id is not null then '1' else '0' end) as like_yn  
+        from field a  
+        left outer join field_like b on b.field_id = a.field_id and b.email = '${email}'
+        where 1=1 
+        and a.field_id = '${fieldId}'`;
     return sql;
 };
 
 //구장 등록
 const insertField = (fieldNm:string, fieldTp:string, area:string, addr:string, lat:number, lng:number, openingHours:string, closingHours:string, price:string, hours:string, note:string, size:string, swrmYn:string, parkingTp:string, rentalSup:string)=> {
-    const sql = "INSERT INTO field " +
-        "field_id, " +
-        "field_nm, " +
-        "field_tp, " +
-        "area, " +
-        "addr, " +
-        "lat, " +
-        "lng, " +
-        "opening_hours, " +
-        "closing_hours, " +
-        "price, " +
-        "hours, " +
-        "note, " +
-        "size, " +
-        "swrm_yn, " +
-        "parking_tp, " +
-        "rental_sup, " +
-        "use_yn, " +
-        "remark_txt, " +
-        "insert_datetime, " +
-        "update_datetime) " +
-        " VALUES ('" +
-        "(SELECT IFNULL(MAX(CAST(b.field_id AS unsigned)) + 1, 1) FROM field b), '" +
-        fieldNm + "' , '" +
-        fieldTp + "', '" +
-        area + "', '" +
-        addr + "', '" +
-        lat + "', '" +
-        lng + "', '" +
-        openingHours + "', '" +
-        closingHours + "', '" +
-        price + "', '" +
-        hours + "', '" +
-        note + "', '" +
-        size + "', '" +
-        swrmYn + "', '" +
-        parkingTp + "', '" +
-        rentalSup + "', " +
-        "'1', " +
-        "NULL, DATE_ADD(NOW(), INTERVAL 9 HOUR), DATE_ADD(NOW(), INTERVAL 9 HOUR))";
+    const sql = `INSERT INTO field
+        field_id,
+        field_nm,
+        field_tp,
+        area,
+        addr,
+        lat,
+        lng,
+        opening_hours,
+        closing_hours,
+        price,
+        hours,
+        note,
+        size,
+        swrm_yn,
+        parking_tp,
+        rental_sup,
+        use_yn,
+        remark_txt,
+        insert_datetime,
+        update_datetime)
+         VALUES (
+        (SELECT IFNULL(MAX(CAST(b.field_id AS unsigned)) + 1, 1) FROM field b), 
+        '${fieldNm}', 
+        '${fieldTp}', 
+        '${area}', 
+        '${addr}', 
+        '${lat}', 
+        '${lng}', 
+        '${openingHours}', 
+        '${closingHours}', 
+        '${price}', 
+        '${hours}', 
+        '${note}', 
+        '${size}', 
+        '${swrmYn}', 
+        '${parkingTp}', 
+        '${rentalSup}',
+        '1',
+        NULL, DATE_ADD(NOW(), INTERVAL 9 HOUR), DATE_ADD(NOW(), INTERVAL 9 HOUR))`;
     return sql;
 }
 
 //구장 수정
-const updateField = (fieldId:string, fieldNm:string, fieldTp:string, area:string, addr:string, lat:number, lng:number, openingHours:string, closingHours:string, price:string, hours:string, note:string, size:string, swrmYn:string, parkingTp:string, rentalSup:string)=> {
-    const sql = "UPDATE field set " +
-    "field_nm = '" + fieldNm + "', " +
-    "field_tp = '" + fieldTp + "', " +
-    "area = '" + area + "', " +
-    "addr = '" + addr + "', " +
-    "lat = " + lat + ", " +
-    "lng = " + lng + ", " +
-    "opening_hours = '" + openingHours + "', " +
-    "closing_hours = '" + closingHours + "', " +
-    "price = " + price + ", " +
-    "hours = '" + hours + "', " +
-    "note = '" + note + "', " +
-    "size = '" + size + "', " +
-    "swrm_yn = '" + swrmYn + "', " +
-    "parking_tp = '" + parkingTp + "', " +
-    "rental_sup = '" + rentalSup + "', " +
-    "update_datetime = DATE_ADD(NOW(), INTERVAL 9 HOUR) " +
-    "WHERE field_id = '" + fieldId + "'";
+const updateField = (fieldId:string, fieldNm:string, fieldTp:string, area:string, addr:string, lat:number, lng:number, openingHours:string, closingHours:string, price:number, hours:string, note:string, size:string, swrmYn:string, parkingTp:string, rentalSup:string)=> {
+    const sql = `UPDATE field set 
+    field_nm = '${fieldNm}', 
+    field_tp = '${fieldTp}', 
+    area = '${area}', 
+    addr = '${addr}', 
+    lat =  ${lat}, 
+    lng =  ${lng}, 
+    opening_hours = '${openingHours}', 
+    closing_hours = '${closingHours}', 
+    price =  ${price}, 
+    hours = '${hours}', 
+    note = '${note}', 
+    size = '${size}', 
+    swrm_yn = '${swrmYn}', 
+    parking_tp = '${parkingTp}', 
+    rental_sup = '${rentalSup}', 
+    update_datetime = DATE_ADD(NOW(), INTERVAL 9 HOUR) 
+    WHERE field_id = '${fieldId}'`;
     return sql;
 }
 
 //구장 삭제
 const deleteField = (fieldId:string)=> {
-    const sql = "DELETE FROM field where field_id = '" + fieldId + "'";
+    const sql = `DELETE FROM field where field_id = '${fieldId}'`;
     return sql;
 }
 
 
 //구장좋아요 조회
 const getFieldLike = (fieldId:string, email:string)=>{
-    let sql = "select a.like_id "+
-        ", a.field_id "+
-        ", a.email " +
-        "from field_like a " +
-        "where 1=1 ";
+    let sql = `select a.like_id
+        , a.field_id
+        , a.email 
+        from field_like a 
+        where 1=1 `;
     if(fieldId) sql = sql + " and field_id = '" + fieldId + "' ";
     if(email) sql = sql + " and email = '" + email + "' ";
     return sql;
