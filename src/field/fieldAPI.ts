2141e1edc5fb62d536f2f369529c02452e754710
csw <csw5050@hanmail.net>
2023-12-31 10:19:50 +0900

    api

--- src/field/fieldAPI.ts
+++ src/field/fieldAPI.ts
@@ -892,11 +892,11 @@ field.post(
     try {
       const param = JSON.parse(JSON.stringify(req.body));
       const fieldDB = require("../field/fieldDB");
-      const fieldId = param["fieldId"];
-      const resvId = param["resvId"];
-      const email = param["email"];
-      const starCnt = param["starCnt"];
-      const reviewCon = param["reviewCon"];
+      const fieldId = param["fieldId"] || null;
+      const resvId = param["resvId"] || null;
+      const email = param["email"] || null;
+      const starCnt = param["starCnt"] || null;
+      const reviewCon = param["reviewCon"] || null;
       let sql = fieldDB.insertFieldReview(
         fieldId,
         resvId,
